5e35020231b95b6e0fb6d6ac7d1d85a179460f2f
binbalz50 <huyducender04@gmail.com>
2025-01-08 03:46:13 +0700

    cris_mesh.py

--- cris_mesh.py
+++ cris_mesh.py
@@ -2,36 +2,49 @@ import os
 import shutil
 import subprocess
 from PyQt6 import QtWidgets
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import Qt, QThread, pyqtSignal
 
-class MeshGenerator: 
+class MeshGenerator(QThread):
+    mesh_generated = pyqtSignal(dict)
+    error_signal = pyqtSignal(str)
+    
+    def __init__(self,group,type):
+        super().__init__()
+        self.code = self.naca_code(group,type)
+
+    @staticmethod 
     def naca_code(group=None,type=None):
         if group=="NACA 4 digit":
             code=type[-4:]
         return code
-    def gen_mesh(code): #generate mesh and post-processing mesh
-        folder_name= f'NACA_{code}'
+    
+    def run(self):
+        self.gen_mesh()
+
+    def gen_mesh(self): #generate mesh and post-processing mesh
+        folder_name= f'NACA_{self.code}'
 
         if not os.path.exists(folder_name): #Create folder
             os.makedirs(folder_name)
         else:
             shutil.rmtree(folder_name)
             os.makedirs(folder_name)
-        mesh_vtk=os.path.join(folder_name, f'mesh_airfoil_{code}.vtk')
-        mesh_su2=os.path.join(folder_name, f'mesh_airfoil_{code}.su2')
-        subprocess.run(['gmshairfoil2d', '--format', 'vtk', '--naca', code, '--output', folder_name, '--farfield', '10'], check=True)
-        subprocess.run(['gmshairfoil2d', '--format', 'su2', '--naca', code, '--output', folder_name, '--farfield', '10'], check=True)
+        subprocess.run(['gmshairfoil2d', '--format', 'vtk', '--naca', self.code, '--output', folder_name, '--farfield', '10'], check=True) 
+        subprocess.run(['gmshairfoil2d', '--format', 'su2', '--naca', self.code, '--output', folder_name, '--farfield', '10'], check=True)
+        mesh_vtk=os.path.join(folder_name, f'mesh_airfoil_{self.code}.vtk')
+        mesh_su2=os.path.join(folder_name, f'mesh_airfoil_{self.code}.su2')
         if os.path.exists(mesh_vtk) and os.path.exists(mesh_su2) and os.path.exists(folder_name):
-            return {
+            print(mesh_vtk)
+            self.mesh_generated.emit( {
                 "mesh_vtk": mesh_vtk,
                 "mesh_su2": mesh_su2,
-                "folder_name": folder_name,
-            }
+            })
         else:
             msg=QtWidgets.QMessageBox()
             msg.setWindowFlags(msg.windowFlags() | Qt.WindowType.WindowStaysOnTopHint)
             msg.setInformativeText('Failed!')
-            msg.exec()  
+            msg.exec() 
 
+        
         
         
