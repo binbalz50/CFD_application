b8aa3674ec76412cbdfa7949781a6d9e6527c462
Cris Nguyen <huyducender04@gmail.com>
2025-05-05 20:58:20 +0700

    Update visual.py

--- visual.py
+++ visual.py
@@ -30,12 +30,43 @@ class VisualizerWidget(QtWidgets.QWidget):
         self.plot.camera.zoom(1.3)
         self.plot.screenshot(rf"NACA_{code}\NACA_{code}_mesh.png",window_size=[1920, 1080])
 
-    def show(self,data,field,code):
+    def show(self,data,field,code,dim):
         result=pv.read(data)
-        field_name=result[field]
-        self.plot.clear()
-        self.plot.add_mesh(result, scalars=field_name, scalar_bar_args={'title': f"{field}" , 'vertical': False}, cmap='jet', show_scalar_bar=True)
-        self.plot.view_xy()
-        self.plot.camera.zoom(1.3)
-        self.plot.screenshot(rf"NACA_{code}\NACA_{code}_{field}.png",window_size=[1080, 1080])
+        
+        if field.lower() == "streamlines":
+            # Assuming the vector field is called "Momentum"
+            vectors_name = "Velocity"
+            if vectors_name not in result.array_names:
+                raise ValueError(f"'{vectors_name}' vector field not found in the data.")
+
+            # Create a line source for seeding the streamlines
+            seed = pv.Line(
+                pointa=(-1.0, -0.1, 0.0),
+                pointb=(2.0, 0.1, 0.0),
+                resolution=100
+            )
+            streamlines = result.streamlines_from_source(
+                seed,
+                vectors=vectors_name,
+                integrator_type=45,  # Runge-Kutta 4-5
+                integration_direction='both',
+                max_time=100.0,
+                initial_step_length=0.5,
+                terminal_speed=1e-12
+            )
+            self.plot.clear()
+            self.plot.add_mesh(result, color='white', opacity=0.3)
+            self.plot.add_mesh(streamlines, scalars='Velocity', cmap='jet', line_width=2)
+
+            self.plot.view_xy()
+            self.plot.camera.zoom(1.3)
+            self.plot.screenshot(rf"NACA_{code}\NACA_{code}_streamlines.png", window_size=[1080, 1080])
+
+
+        else:
+            self.plot.clear()
+            self.plot.add_mesh(result, scalars=result[field], scalar_bar_args={'title': f"{field} ({dim})" , 'vertical': False}, cmap='jet', show_scalar_bar=True)
+            self.plot.view_xy()
+            self.plot.camera.zoom(1.3)
+            self.plot.screenshot(rf"NACA_{code}\NACA_{code}_{field}.png",window_size=[1080, 1080])
 
